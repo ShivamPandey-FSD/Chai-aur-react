ad2e1328b476085e46a7ec7d4491856b8783cf93
Shivam Pandey <shivampandey4597@gmail.com>
2024-07-23 17:16:44 +0530

    redux toolkit

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
