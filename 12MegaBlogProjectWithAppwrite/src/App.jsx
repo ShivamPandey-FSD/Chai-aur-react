9548ec1cc175cb5398c31beb412acdd85b7dbc4e
Shivam Pandey <shivampandey4597@gmail.com>
2024-08-01 09:36:08 +0530

    Blog Application Project Part-2

--- 12MegaBlogProjectWithAppwrite/src/App.jsx
+++ 12MegaBlogProjectWithAppwrite/src/App.jsx
@@ -1,12 +1,48 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import loadingLogo from './assets/loading_logo.svg';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog app with Appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        throw error;
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div className="min-h-[90vh] flex justify-center items-center">
+      <img src={loadingLogo} alt="loading" className="bg-transparent" />
+    </div>
+  );
 }
 
-export default App
+export default App;
